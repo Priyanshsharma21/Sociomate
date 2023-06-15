65b27329bd5d95f883ae7b98d922ee54f15b54b1
Priyanshsharma21 <piyuindia4@gmail.com>
2023-06-16 00:19:12 +0530

    sociomate day1

---
+++ server/src/app.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import userRoutes from './routes/userRoutes.js'
+import postRoutes from './routes/postRoutes.js'
+
+
+const app = express()
+
+
+// global middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+// cors middleware
+app.use(cors())
+
+// logging middleware
+app.use(morgan("tiny"))
+
+
+// testing route
+app.get('/',(req,res)=>{
+    res.status(200).json({status : true, message : "SOCIOMATE IS RUNNING LIKE BOLT"})
+})
+
+
+// routing middlewares
+app.use('/api/v1', userRoutes)
+app.use('/api/v1', postRoutes)
+
+// http://localhost:3000/api/v1/user
+
+
+export default app
