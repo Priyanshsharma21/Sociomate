4c3a1c0b9c11baeeef6d2bce0ea8590fa08c4f8b
Pal Bijewar <pal.bijewar@gmail.com>
2023-06-17 13:13:51 +0530

    random commit to pull

---
+++ server/src/controllers/posts.js
@@ -0,0 +1,12 @@
+// import Posts from '../models/postsModel';
+
+// export const posting = async(req,res)=>{
+//     try {
+//         const {}
+//     } catch (error) {
+//         res.status(500).json({
+//             status: false,
+//             error: error.message
+//           });
+//     }
+// }
--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -179,3 +179,4 @@ export const profileUpdate = async(req,res)=>{
     });
   }
 };
+
